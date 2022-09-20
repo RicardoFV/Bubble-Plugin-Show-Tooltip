0c484ef1debdcede99c402b5a55115211d0c0228
RicardoFV <48629398+RicardoFV@users.noreply.github.com>
2022-09-20 07:01:51 -0300

    Automatic commit from Bubble

--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -1,56 +1,61 @@
 function(instance, properties, context) {
-	// pega o id do elemento
-	let elemento = document.getElementById(properties.id)
-    // guarda a posição que foi selecionada pelo o usuario
-    let posicao; 
-  	// define a cor da seta
-    let posicao_css;
-    // guarda a osição da mensagem
-    let posicao_mensagem;
-    // define a borda do elemento
-    let borda = ''
     
-    // trata a seleção das opções do elemento
-    switch(properties.posicao){
-        case "Topo":
-            posicao = 'top';
-            posicao_css = 'top';
-            break;
+    // verifica se o elemento existe
+	let elementoExiste = document.body.contains(document.getElementById(properties.id))
+    // caso o id informado seja diferente de null e o elemento exista
+    if(properties.id !== null && elementoExiste){ //inicio do if 
+    	// pega o id do elemento
+		let elemento = document.getElementById(properties.id)		
+    	// guarda a posição que foi selecionada pelo o usuario
+    	let posicao; 
+  		// define a cor da seta
+    	let posicao_css;
+    	// guarda a osição da mensagem
+    	let posicao_mensagem;
+    	// define a borda do elemento
+    	let borda = ''
+    
+    	// trata a seleção das opções do elemento
+    	switch(properties.posicao){
+        	case "Topo":
+            	posicao = 'top';
+            	posicao_css = 'top';
+            	break;
             
-        case "Rodapé":
-            posicao = 'bottom';
-            posicao_css = 'bottom';
-            break;
+        	case "Rodapé":
+            	posicao = 'bottom';
+            	posicao_css = 'bottom';
+            	break;
                      
-        case "Esquerda":
-            posicao = "left";
-            posicao_css = 'start';
-            break;
+        	case "Esquerda":
+            	posicao = "left";
+            	posicao_css = 'start';
+            	break;
             
-        default:
-            posicao = 'right';
-            posicao_css = 'end';
-        }
+        	default:
+            	posicao = 'right';
+            	posicao_css = 'end';
+        	}
     
-    // trata a seleção das opções da posição da mensagem
-    switch(properties.posicao_mensagem){      
-        case "Centro":
-            posicao_mensagem = 'center' 
-            break;
+    	// trata a seleção das opções da posição da mensagem
+    	switch(properties.posicao_mensagem){      
+        	case "Centro":
+            	posicao_mensagem = 'center' 
+            	break;
             
-        case "Direita":
-            posicao_mensagem = 'end'
-            break;
+        	case "Direita":
+            	posicao_mensagem = 'end'
+            	break;
             
-        default:
-            posicao_mensagem = 'start'
-        }
+        	default:
+            	posicao_mensagem = 'start'
+        	}
     
-    // verfiica se a borda esta marcado como sim ou não
-    properties.borda == true ? borda = '1px solid rgba(0, 0, 0, .2)' : borda = 'none'
+    	// verfiica se a borda esta marcado como sim ou não
+    	properties.borda == true ? borda = '1px solid rgba(0, 0, 0, .2)' : borda = 'none'
      
-    // trata o tamanho da fonte e a sua opacidade
-    let style = $(`<style> 
+    	// trata o tamanho da fonte e a sua opacidade
+    	let style = $(`<style> 
 		.popover{
 			font-size:${properties.tamanho_fonte_corpo}px;
 			line-height: 0.1;
@@ -71,20 +76,20 @@ function(instance, properties, context) {
 		}
 	</style>`)
     
-    // insere o estilo
-    $(style).appendTo('head')
-	// cria as options
-    let options = {
-        title :properties.titulo , 
-        content : properties.mensagem,
-        placement : posicao 
-    }
+    	// insere o estilo
+    	$(style).appendTo('head')
+		// cria as options
+    	let options = {
+    	    title :properties.titulo , 
+        	content : properties.mensagem,
+        	placement : posicao 
+    	}
     
-    // insere no elemento o que foi configurado como ação
-	let popover = new bootstrap.Popover(elemento, options)
-    // seleciona a ação que sera feita
-    switch(properties.acao){
-        case "click": 
+    	// insere no elemento o que foi configurado como ação
+		let popover = new bootstrap.Popover(elemento, options)
+    	// seleciona a ação que sera feita
+    	switch(properties.acao){
+        	case "click": 
             	// aparece a mensagem quando o mouse é passado em cima do elemento
     			elemento.addEventListener('onClick', function(){
         		popover.show()    
@@ -94,7 +99,7 @@ function(instance, properties, context) {
         		popover.hide()    
     			})
         		break
-        default :
+        	default :
         		// aparece a mensagem quando o mouse é passado em cima do elemento
     			elemento.addEventListener('mouseover', function(){
         		popover.show()    
@@ -103,5 +108,6 @@ function(instance, properties, context) {
     			elemento.addEventListener('mouseleave', function(){
         		popover.hide()    
     			})
-    	}    
+    		}
+		} // fim do if
 }
